4b66cb9bf473930b0796f0a35ff50fb928233602
egor <huravltampl@gmail.com>
2020-09-25 23:50:31 +0600

    coomit

--- delete.php
+++ delete.php
@@ -1,3 +1,14 @@
 <?php
-/// proba pera
-/// second proba pera
+
+include('connect.php');
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM employeer WHERE id='$id'";
+
+if ($conn->query($sql) === TRUE) {
+    echo '<script>console.log("record success delete!")</script>';
+}
+else {
+    echo "error delete record:"    .$conn->error;
+}
