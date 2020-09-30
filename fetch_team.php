fdb19dca51ed64d666f45ea969465c85ebea1635
egor <huravltampl@gmail.com>
2020-10-01 03:40:14 +0600

    team is done

--- fetch_team.php
+++ fetch_team.php
@@ -7,7 +7,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc())     {
-        echo "<tr><td>".$row['title_team']. "</td><td>".$row['number_of_employees']. "</td><td>".$row['max_salary']. "</td>";
+        echo "<tr><td>".$row['team']. "</td><td>".$row['number_empl']. "</td><td>".$row['max_salary']. "</td>";
                    }
 }
 else {
